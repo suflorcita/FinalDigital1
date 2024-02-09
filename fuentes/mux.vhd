46b54632155d44d6af888a7d4f89a666c154b5b9
Sol Cataldo <solcataldo98@gmail.com>
2024-02-09 11:12:45 -0300

    Changes in multiplexor

--- fuentes/mux.vhd
+++ fuentes/mux.vhd
@@ -8,9 +8,13 @@ entity mux is
 		digito1: in bit_vector(3 downto 0); -- Este es el primer dígito (4 bits: del 0 al 9, Es lo que sale del registro)
 		digito2: in bit_vector(3 downto 0); -- Segundo dígito (Decimal)
 		digito3: in bit_vector(3 downto 0); -- Tercer dígito (Centesima)
+		
 		-- Caracteres--
-		punto: in bit_vector(3 downto 0); -- El punto. Le vamos asignar el valor 10d (1010b)
-		letra_v: in bit_vector(3 downto 0); -- La letra V de Volts. Le vamos asignar el valor 11d (1011b)
+--		punto: in bit_vector(3 downto 0); -- El punto. Le vamos asignar el valor 10d (1010b)
+--		letra_v: in bit_vector(3 downto 0); -- La letra V de Volts. Le vamos asignar el valor 11d (1011b)
+		
+		-- Espacio en blanco--
+--		blanco: in bit_vector(3 downto 0); 	
 		
 		-- Selector --
 		selector: in bit_vector(2 downto 0); -- Selector de 3 bits porque elige entre 5 entradas. 
@@ -22,7 +26,7 @@ entity mux is
 end; 
 
 architecture mux_arq of mux is
-	signal posicion: bit_vector(4 downto 0); -- Representa que posición estoy eligiendo. Tengo 5 posibilidades
+	signal posicion: bit_vector(6 downto 0); -- Representa que posición estoy eligiendo. Tengo 6 posibilidades
 	
 	-- Señales de salida del and de los dígitos, el punto y la letra
 	signal salida_digito1: bit_vector(3 downto 0); -- Cada una tiene 4 dígitos
@@ -30,6 +34,7 @@ architecture mux_arq of mux is
 	signal salida_digito3: bit_vector(3 downto 0); 
 	signal salida_punto: bit_vector(3 downto 0); 
 	signal salida_letra_v: bit_vector(3 downto 0);
+	signal salida_blanco: bit_vector(3 downto 0);
 	 
 
 begin 
@@ -39,6 +44,7 @@ begin
 	posicion(2) <= not selector(2) and selector(1) and not selector(0); -- Tercera posición (010b). D3
 	posicion(3) <= not selector(2) and selector(1) and selector(0); -- Cuarto posición (011b). . 
 	posicion(4) <= selector(2) and not selector(1) and not selector(0); -- Quinta posición (100b): Letra V
+	posicion(5) <= selector(2) and not selector(1) and  selector(0); -- Sexta posición (101b): Blanco
 	
 	-- Comparo cada entrada con la selección
 	salida_digito1 <= (digito1(3) and posicion(0)) & 
@@ -55,19 +61,26 @@ begin
 		          (digito3(2) and posicion(2)) & 
 		          (digito3(1) and posicion(2)) & 
 		          (digito3(0) and posicion(2));
-		          
-	salida_punto <= (punto(3) and posicion(3)) & 
-		        (punto(2) and posicion(3)) & 
-		        (punto(1) and posicion(3)) & 
-		        (punto(0) and posicion(3));
+	-- Punto hardcodeo un 10: '1010'
+	salida_punto <= ('1' and posicion(3)) & 
+		        ('0' and posicion(3)) & 
+		        ('1' and posicion(3)) & 
+		        ('0' and posicion(3));
 
-	salida_letra_v <= (letra_v(3) and posicion(4)) & 
-		          (letra_v(2) and posicion(4)) & 
-		          (letra_v(1) and posicion(4)) & 
-		          (letra_v(0) and posicion(4));
-
+	-- Volt hardcodeo un 11: '1011'
+	salida_letra_v <= ('1' and posicion(4)) & 
+		          ('0' and posicion(4)) & 
+		          ('1' and posicion(4)) & 
+		          ('1' and posicion(4));
+	
+	-- Blanco hardcodeo un 12: '1100'
+	salida_blanco <= ('1' and posicion(5)) & 
+		          ('1' and posicion(5)) & 
+		          ('0' and posicion(5)) & 
+		          ('0' and posicion(5));	              
+		      
 	-- Salida del multiplexor: Me va a quedar la que no den todos 0
-	mux_out <= salida_digito1 or salida_digito2 or salida_digito3 or salida_punto or salida_letra_v; 
+	mux_out <= salida_digito1 or salida_digito2 or salida_digito3 or salida_punto or salida_letra_v or salida_blanco ; 
 	
 end;
 
--- fuentes/mux_tb.vhd
+++ fuentes/mux_tb.vhd
@@ -13,9 +13,13 @@ architecture mux_tb_arq of mux_tb is
 			digito1: in bit_vector(3 downto 0); -- Este es el primer dígito (4 bits: del 0 al 9, Es lo que sale del registro)
 			digito2: in bit_vector(3 downto 0); -- Segundo dígito (Decimal)
 			digito3: in bit_vector(3 downto 0); -- Tercer dígito (Centesima)
+			
 			-- Caracteres--
-			punto: in bit_vector(3 downto 0); -- El punto. Le vamos asignar el valor 10d (1010b)
-			letra_v: in bit_vector(3 downto 0); -- La letra V de Volts. Le vamos asignar el valor 11d (1011b)
+--			punto: in bit_vector(3 downto 0); -- El punto. Le vamos asignar el valor 10d (1010b)
+--			letra_v: in bit_vector(3 downto 0); -- La letra V de Volts. Le vamos asignar el valor 11d (1011b)
+			
+			-- Espacio en blanco--
+--			blanco: in bit_vector(3 downto 0); 
 			
 			-- Selector --
 			selector: in bit_vector(2 downto 0); -- Selector de 3 bits porque elige entre 5 entradas. 
@@ -31,27 +35,33 @@ architecture mux_tb_arq of mux_tb is
 	signal digito1_tb : bit_vector(3 downto 0) := "0010";
 	signal digito2_tb : bit_vector(3 downto 0) := "0011";
 	signal digito3_tb : bit_vector(3 downto 0) := "0111";
-	signal punto_tb: bit_vector(3 downto 0):="1010"; 
-	signal letra_v_tb : bit_vector(3 downto 0) := "1011";
+--	signal punto_tb: bit_vector(3 downto 0):="1010"; 
+--	signal letra_v_tb : bit_vector(3 downto 0) := "1011";
+--	signal blanco_tb : bit_vector(3 downto 0) := "1100";
 	
 	-- Selector
 	signal selector_tb: bit_vector(2 downto 0) := "000";
 	
 	-- Salida
 	signal mux_out_tb: bit_vector(3 downto 0);
+	
+	-- El reloj es solo para ver las salidas 
+	signal clk_tb: bit := '1';
 
 
 begin
-    selector_tb <= "001" after 500 ns, "010" after 1000 ns, "011" after 1500 ns, "100" after 2000 ns, "000" after 2500 ns;
+	clk_tb <= not clk_tb after 20 ns;	
+	
+    selector_tb <= "001" after 500 ns, "010" after 1000 ns, "011" after 1500 ns, "100" after 2000 ns, "101" after 2500 ns;
 
     DUT : mux
         port map (
             digito1 => digito1_tb, 
             digito2 => digito2_tb,
             digito3 => digito3_tb,
-            punto  => punto_tb,
-            letra_v => letra_v_tb, 
-            selector => selector_tb, 
+   --         punto  => punto_tb,
+   --         letra_v => letra_v_tb, 
+            selector => selector_tb,
             mux_out => mux_out_tb
         );
 
